b9db0f4a011703a4a27c75c7f6bc61623a99953d
AbdullohKomil <akomiljonov@icloud.com>
2022-12-03 09:48:11 +0500

    loader navbar-shrink and backtop changed

--- js/index.js
+++ js/index.js
@@ -46,16 +46,50 @@ btn_left.addEventListener("click", function () {
   imglorem.classList.toggle("dsp_none");
 });
 
-  var clicked = document.querySelectorAll(".buttun");
-  let editclass = document.querySelectorAll(".accordion-left");
-  clicked.forEach(clicced => {
-    clicced.addEventListener("click", (e)=> {
-      console.log( );
-      editclass.forEach(toggler=> {
-        if(e.target.id == toggler.id){
-        toggler.classList.toggle("heightmax")}
-      })
-    })
-  })
- 
+var clicked = document.querySelectorAll(".buttun");
+let editclass = document.querySelectorAll(".accordion-left");
+clicked.forEach((clicced) => {
+  clicced.addEventListener("click", (e) => {
+    console.log();
+    editclass.forEach((toggler) => {
+      if (e.target.id == toggler.id) {
+        toggler.classList.toggle("heightmax");
+      }
+    });
+  });
+});
 
+let loader = document.querySelector(".div_loading");
+let body = document.body;
+let html = document.querySelector(".html");
+html.style.overflow = "hidden";
+function load() {
+  loader.style.display = "none";
+  html.style.overflow = "auto";
+}
+
+function loadersa() {
+  let loaders = setTimeout(load, 1500);
+}
+// console.log(html);
+window.addEventListener("scroll", (event) => {
+  if (window.scrollY > 1) {
+    document.querySelector("header").style.boxShadow =
+      "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px";
+    document.querySelector(".nav_inner").style.height = "65px";
+    document.querySelector(".backtop").style.display = "flex";
+  }else {
+    document.querySelector("header").style.boxShadow = "none";
+    document.querySelector(".nav_inner").style.height = "";
+    document.querySelector(".backtop").style.display = "none";
+  }
+  if (window.scrollY > 5) {
+    // document.querySelector(".backtop").style.height = "50px";
+    // document.querySelector(".backtop").style.width = "50px";
+    document.querySelector(".backtop").style.padding = "20px 15px";
+  } else {
+    // document.querySelector(".backtop").style.height = "0px";
+    // document.querySelector(".backtop").style.width = "0px";    
+    document.querySelector(".backtop").style.padding = "0px";
+  }
+});
